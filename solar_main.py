16c72da87ed675387752f8ead9e75faa7b68b310
Zuev-534 <zuev,andrey.2103@g,ail.com>
2021-11-22 22:42:49 +0300

    Допилил графики(фин.04)

--- solar_main.py
+++ solar_main.py
@@ -175,7 +175,9 @@ def main():
     height = user32.GetSystemMetrics(1) - 55
     screen = pg.display.set_mode((width, height))
     with open("stats.txt", 'r') as input_file:
-        last_time = int((input_file[0].split())[1])
+        for line in input_file:
+            last_time = int((line.split())[1])
+            break
     drawer = Drawer(screen)
     menu, box, timer = init_ui(screen)
     perform_execution = True
